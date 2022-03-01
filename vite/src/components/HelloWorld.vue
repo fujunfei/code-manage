8abc0c5bbc760a7239c1152199b0955b043a8ab3
fujunfei <fujunfei@flashexpress.com>
2022-03-01 21:06:39 +0800

    vite模板

---
+++ vite/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useMouse, usePreferredDark, useLocalStorage, useBreakpoints } from '@vueuse/core'
+
+const breakpoints = useBreakpoints({
+  tablet: 640,
+  laptop: 524,
+  desktop: 3080,
+})
+
+const laptop = breakpoints.between('laptop', 'desktop')
+console.log(laptop.value);
+
+debugger
+
+const { x, y } = useMouse()
+</script>
+
+<template>
+  <div>pos: {{x}}, {{y}}</div>
+</template>
